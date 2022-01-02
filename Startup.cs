ccc23191b2b7b72f44a0cd155b7904b1c8ef9eea
Damir Safarov <damir.safarov.gdc@fujitsu.com>
2022-01-02 22:19:41 +0300

    Chapter 13 - Using a catchall Segment Variable

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace Platform
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("files/{filename}.{ext}", async context =>
+                endpoints.MapGet("{first}/{second}/{*catchall}", async context =>
                 {
                     await context.Response.WriteAsync("Request Was Routed\n");
                     foreach (var kvp in context.Request.RouteValues)
