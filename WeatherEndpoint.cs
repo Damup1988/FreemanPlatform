df42b85950f9c09d56f7941c411c5405de086d63
Damir Safarov <damir.safarov.gdc@fujitsu.com>
2022-01-04 13:39:58 +0300

    Chapter 14 - Creating a Middleware Component and an Endpoint

---
+++ WeatherEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Platform
+{
+    public class WeatherEndpoint
+    {
+        public static async Task Endpoint(HttpContext context)
+        {
+            await context.Response.WriteAsync("Endpoint Class: It is cloudy in Milan");
+        }
+    }
+}
---
+++ WeatherMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Platform
+{
+    public class WeatherMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WeatherMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/middleware/class")
+            {
+                await context.Response.WriteAsync("Middleware Class: It is raining in London");
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
