d4ce9be7ce9e5bc6db156d81191d1c7d5faefae0
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-01-21 00:11:33 +0300

    Chapter 14 - Creating New Handlers for Each Request

--- Services/EndPointExtensions.cs
+++ Services/EndPointExtensions.cs
@@ -21,10 +21,13 @@ namespace Microsoft.AspNetCore.Builder
             T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
 
             ParameterInfo[] methodParams = methodInfo.GetParameters();
-            app.MapGet(path, context => (Task)methodInfo.Invoke(
-                endpointInstance, methodParams.Select(p => p.ParameterType == typeof(HttpContext)
-                    ? context
-                    : context.RequestServices.GetService(p.ParameterType)).ToArray()));
+            app.MapGet(path, context =>
+            {
+                T endpointInstance = ActivatorUtilities.CreateInstance<T>(context.RequestServices);
+                return (Task)methodInfo.Invoke(endpointInstance, methodParams.Select(p => 
+                p.ParameterType == typeof(HttpContext) ? context :
+                    context.RequestServices.GetService(p.ParameterType)).ToArray());
+            });
         }
     }
 }
