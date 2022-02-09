78691f2d29a975233b5a0bf8b02ecd9fdc8e2ff2
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-02-09 23:58:06 +0300

    Chapter 15 - Preparing for This Chapter

--- Startup.cs
+++ Startup.cs
@@ -16,61 +16,19 @@ namespace Platform
 {
     public class Startup
     {
-        private IConfiguration _configuration;
-        
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-        
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(typeof(ICollection<>), typeof(List<>));
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseRouting();
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/middleware/function")
-                {
-                    IResponseFormatter formatter = context.RequestServices.GetService<IResponseFormatter>();
-                    if (formatter != null)
-                        await formatter.Format(
-                            context, "Middleware Function: It is snowing in Chicago");
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/string", async context =>
+                endpoints.MapGet("/", async context =>
                 {
-                    ICollection<string> collection = context.RequestServices.GetService<ICollection<string>>();
-                    if (collection != null)
-                    {
-                        collection.Add($"Request: {DateTime.Now.ToLongTimeString()}");
-                        foreach (string str in collection)
-                        {
-                            await context.Response.WriteAsync($"String: {str}\n");
-                        }
-                    }
-                });
-
-                endpoints.MapGet("/int", async context =>
-                {
-                    ICollection<int> collection = context.RequestServices.GetService<ICollection<int>>();
-                    if (collection != null) collection.Add(collection.Count() + 1);
-                    foreach (int val in collection)
-                    {
-                        await context.Response.WriteAsync($"Int: {val}\n");
-                    }
+                    await context.Response.WriteAsync("Hello World");
                 });
             });
         }
