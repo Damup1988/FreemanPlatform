fe1c1ea56719ad208e7dae0dccc567d83f3a3298
Damir Safarov <damup1988@gmail.com>
2022-04-02 16:55:26 +0300

    Chapter 16 - Managing Cookie Consent

---
+++ ConsentMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Platform
+{
+    public class ConsentMiddleware
+    {
+        private RequestDelegate next;
+
+        public ConsentMiddleware(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/consent")
+            {
+                ITrackingConsentFeature consentFeature = context.Features.Get<ITrackingConsentFeature>();
+                if (!consentFeature.HasConsent)
+                {
+                    consentFeature.GrantConsent();
+                }
+                else
+                {
+                    consentFeature.WithdrawConsent();
+                }
+
+                await context.Response.WriteAsync(consentFeature.HasConsent
+                    ? "Consent Granted \n"
+                    : "Consent Withdrawn\n");
+            }
+
+            await next(context);
+        }
+    }
+}
