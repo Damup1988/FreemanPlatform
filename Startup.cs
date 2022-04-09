cf8f45931260e3ba92a35d19a3da8c3c77a659d2
Damir Safarov <damup1988@gmail.com>
2022-04-09 23:58:54 +0300

    Chapter 16 - Handling Exceptions and Errors

--- Startup.cs
+++ Startup.cs
@@ -51,29 +51,8 @@ namespace Platform
             app.UseCookiePolicy();
             app.UseMiddleware<ConsentMiddleware>();
             app.UseSession();
-            app.UseRouting();
-
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
-                await next();
-            });
             
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/cookie", async context =>
-                {
-                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                    context.Session.SetInt32("counter1", counter1);
-                    context.Session.SetInt32("counter2", counter2);
-                    await context.Session.CommitAsync();
-                    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
-                
-                endpoints.MapFallback(async context =>
-                    await context.Response.WriteAsync("Hello world!"));
-            });
+            app.Run(context => throw new Exception("Something has gone wrong"));
         }
     }
 }
