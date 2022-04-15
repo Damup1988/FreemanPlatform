3dc938f31996329a9914b7918547936f630a4856
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-04-15 16:03:32 +0300

    Chapter 17 - Configuring the Database Service

--- Startup.cs
+++ Startup.cs
@@ -8,11 +8,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Platform.Models;
 using Platform.Services;
 
 namespace Platform
@@ -36,6 +38,10 @@ namespace Platform
             });
             services.AddResponseCaching();
             services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+            services.AddDbContext<CalculationContext>(opts =>
+            {
+                opts.UseSqlServer(_configuration["ConnectionStrings:CalcConnection"]);
+            });
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
