99a2c47cd3b43a37c0b6a86fdfc4b3818a175434
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-04-15 15:40:15 +0300

    Chapter 17 - Creating the Data Model

---
+++ Models/CalculationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Platform.Models
+{
+    public class CalculationContext : DbContext
+    {
+        public DbSet<Calculation> Calculations { get; set; }
+        
+        public CalculationContext(DbContextOptions<CalculationContext> opts) : base(opts)
+        {
+            
+        }
+    }
+}
