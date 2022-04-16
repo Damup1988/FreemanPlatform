2ff15322f8f72c66bd5254fe787dacdf002219d2
Damir Safarov <damir.safarov.gdc@outlook.com>
2022-04-16 16:16:10 +0300

    Chapter 17 - Using Data in an Endpoint

--- SumEndpoint.cs
+++ SumEndpoint.cs
@@ -1,33 +1,39 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Caching.Distributed;
+using Platform.Models;
 using Platform.Services;
 
 namespace Platform
 {
     public class SumEndpoint
     {
-        public async Task Endpoint(HttpContext context, IDistributedCache cache,
-            IResponseFormatter formatter, LinkGenerator generator)
+        public async Task Endpoint(HttpContext context, CalculationContext calculationContext)
         {
             int count = int.Parse((string)context.Request.RouteValues["count"] ?? string.Empty);
-            long total = 0;
-            for (int i = 0; i < count; i++)
+            long total = calculationContext.Calculations.FirstOrDefault(c => c.Count == count)?.Result ?? 0;
+            if (total == 0)
             {
-                total = +i;
+                for (int i = 0; i < count; i++)
+                {
+                    total += i;
+                }
+
+                calculationContext.Calculations!.Add(new Calculation()
+                {
+                    Count = count,
+                    Result = total
+                });
+                await calculationContext.SaveChangesAsync();
             }
 
-            string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
-            context.Response.Headers["Cache-Control"] = "public, max-age=120";
-            string url = generator.GetPathByRouteValues(context, null, new { count = count });
-
-            await formatter.Format(context,
-                $"<div>({DateTime.Now.ToLongTimeString()}) Total for {count}"
-                + $" values:</div><div>{totalString}</div>"
-                + $"<a href={url}>Reload</a>");
+            string totatlString = $"({DateTime.Now.ToLongTimeString()}) {total}";
+            await context.Response.WriteAsync($"({DateTime.Now.ToLongTimeString()}) Totatl for {count}"
+            + $" values:\n{totatlString}\n");
         }
     }
 }
